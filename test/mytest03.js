8da5dc6a5dab9321e955a567eccd2be1b62c9f7e
ilomus76 <ilomus76@gmail.com>
2026-06-12 00:45:35 +0900

    mytest data 03 upload from home to github

---
+++ test/mytest03.js
@@ -0,0 +1,9 @@
+function onloadBody(){
+    var bd = document.getElementById('bd');
+    bd.style.backgroundColor = '#888';
+
+    var div = document.querySelectorAll('div');
+    // div.style.border = 1px;
+    // div.style.width = '50%'
+    
+}
